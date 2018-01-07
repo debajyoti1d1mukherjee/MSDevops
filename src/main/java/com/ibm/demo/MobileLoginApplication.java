facbd0d93843c209940eff77daee7798cfc578b1
deb <debajmuk@in.ibm.com>
2018-01-07 11:45:41 +0530

    Test deploy

--- src/main/java/com/ibm/demo/MobileLoginApplication.java
+++ src/main/java/com/ibm/demo/MobileLoginApplication.java
@@ -8,6 +8,7 @@ import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.netflix.hystrix.EnableHystrix;
 import org.springframework.data.mongodb.repository.config.EnableMongoRepositories;
 //This is a boot application hook test or polling test
+//Will be deployed in kubernetes
 
 @SpringBootApplication
 @EnableMongoRepositories("com.ibm.demo.repository")
