9feeda1f52911a8b46d68bdee9203eae888833a4
deb <debajmuk@in.ibm.com>
2018-01-07 15:24:36 +0530

    Test pipeline

--- src/main/java/com/ibm/demo/repository/offerMongoRepository.java
+++ src/main/java/com/ibm/demo/repository/offerMongoRepository.java
@@ -3,5 +3,6 @@ package com.ibm.demo.repository;
 import org.springframework.data.repository.CrudRepository;
 
 import com.ibm.demo.model.offerService;
+//Test pipeline
 
  interface offerMongoRepository extends CrudRepository<offerService, String>{}
